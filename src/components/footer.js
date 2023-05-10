60fba91531406e6da3c3090b53d6983f542f5513
edensalt <124738007+edensalt@users.noreply.github.com>
2023-05-10 16:36:21 -0500

    update link to github footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,7 +16,7 @@ const Footer = function () {
 
   const username = document.createElement("p");
   username.setAttribute("id", "username");
-  username.innerHTML = `<a href="https://github.com/edensalt">@edensalt</a>`;
+  username.innerHTML = `<a href="https://github.com/edensalt/restaurant-page">@edensalt</a>`;
   footer.appendChild(username);
 
   return footer;
