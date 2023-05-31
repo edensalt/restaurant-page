43e30bb4fb0f4ac791b31f149d64572bc27f6b49
edensalt <124738007+edensalt@users.noreply.github.com>
2023-05-31 14:05:56 -0500

    Update contact page and carousel proportions

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,9 +11,12 @@ const ContactPage = function () {
   container.setAttribute("id", "contact");
   content.appendChild(container);
 
+  const leftCol = document.createElement('div');
+  container.appendChild(leftCol);
+
   const subtitle = document.createElement("h2");
   subtitle.innerHTML = "Contact us";
-  container.appendChild(subtitle);
+  leftCol.appendChild(subtitle);
 
   const coffeeImg = new Image();
   coffeeImg.src = coffeeImage;
@@ -25,18 +28,19 @@ const ContactPage = function () {
     }, 1800);
   });
   container.appendChild(coffeeImg);
+  
 
   const phone = document.createElement("p");
   phone.innerHTML = `<a href="tel:+1-555-555-5555">555-555-5555</a>`;
-  container.appendChild(phone);
+  leftCol.appendChild(phone);
 
   const email = document.createElement("p");
   email.innerHTML = `<a href="mailto:hello@neoncoffee.com"</a>hello@neoncoffee.com</a>`;
-  container.appendChild(email);
+  leftCol.appendChild(email);
 
   const address = document.createElement("p");
   address.innerHTML = "100 Amazing St, Awesome City, ST";
-  container.appendChild(address);
+  leftCol.appendChild(address);
 
   return container;
 };
